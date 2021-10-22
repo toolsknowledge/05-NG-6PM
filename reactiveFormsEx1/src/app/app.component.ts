ff89842802b5cda798667726bc7b83689bda0874
Sambasivarao <samba.pendela@gmail.com>
2021-10-22 18:54:47 +0530

    reactive forms

--- reactiveFormsEx1/src/app/app.component.ts
+++ reactiveFormsEx1/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +10,48 @@ export class AppComponent {
   title = 'reactiveFormsEx1';
   userDetails:FormGroup;
   address:FormGroup;
+  genders:any[];
+  countries:string[] = ['india','usa','canada','china','japan'];
   constructor(){
     this.userDetails = new FormGroup({
         uname:new FormControl(null,[Validators.required]),
         email:new FormControl(null,[Validators.required,Validators.email]),
         mobile:new FormControl(null,[Validators.required]),
+        gender:new FormControl('female'),
         address:new FormGroup({
             city:new FormControl("Hyderabad",[Validators.required]),
             pincode:new FormControl(null,[Validators.required])
-        })
+        }),
+        country:new FormControl(null),
+        subjects:new FormArray([])
+        
     });
     this.address = <FormGroup>this.userDetails.controls["address"];
+    this.genders = ['male','female'];
   };
+
+
+  addSubjects():void{
+     const control = new FormControl(null);
+     (<FormArray>this.userDetails.controls["subjects"]).push(control);
+  }
+
+
+  get subjectsArray():any{
+      return this.userDetails.controls['subjects'] as FormArray;
+  }
+
+  removeSubject(i:number):any{
+      //filter funtion to remove item
+  }
+
 
   register():void{
       console.log(this.userDetails.value);
   };
+
+
+
 
 
 }
