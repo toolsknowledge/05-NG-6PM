3173cdc5c43e263785823721303f7917c5f69a24
Sambasivarao <samba.pendela@gmail.com>
2021-10-27 18:50:49 +0530

    code

--- moduleEx1/src/app/app.module.ts
+++ moduleEx1/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HrModule } from './hr/hr/hr.module';
+
 @NgModule({
   declarations: [
     AppComponent
