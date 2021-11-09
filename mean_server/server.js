2fcffbfafa127f6d7426f446d4554bcfabadf9e7
Sambasivarao <samba.pendela@gmail.com>
2021-11-09 18:58:32 +0530

    mean server

--- mean_server/server.js
+++ mean_server/server.js
@@ -2,43 +2,65 @@
 //import the modules
 //require() function used to import the modules
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const mongodb = require("mongodb");
 const jwt = require("jwt-simple");
-const user = require("./userModel");
+
 
 //create the rest object
 const app = express();
-//where "app" is the rest object
-//where "app" object used to develop the rest api's
 
 
-//enable cors policy
+//enable the cors policy
 app.use(cors());
 
 
-//set the "json" as MIME type
+//set the json as MIME Type
 app.use(express.json());
 
+//create the reference variable to connect to mongodb database
+const ashokIT = mongodb.MongoClient;
+//where "ashokIT" is the reference variable
+//where "ashokIT" used to connect to the mongodb database
 
-//connect to mongodb database
-mongoose.connect(`mongodb+srv://admin:admin@miniprojectdb.nzphu.mongodb.net/05-ng-6pm?retryWrites=true&w=majority`,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true
+
+
+let server_token = "";
+//create the post request
+app.post("/login",(req,res)=>{
+    ashokIT.connect(`mongodb+srv://admin:admin@miniprojectdb.nzphu.mongodb.net/05-ng-6pm?retryWrites=true&w=majority`,(err,connection)=>{
+        if(err) throw err;
+        else{
+            const db = connection.db("05-ng-6pm");
+            db.collection("user_details").findOne({"email":req.body.email,"password":req.body.password},(err,records)=>{
+                if(err){
+                    res.send({"login":"fail"});
+                }
+                else{
+                   let arr = Object.entries(records);
+                   if(arr.length>0){
+                        //token
+                        //converting readable data to unreadable data with custom key called as token
+                        //custom key used to perform both encryption and decryption
+                        let token = jwt.encode({"email":req.body.email,"password":req.body.password},"hr@ashokit.in");
+                        server_token = token;
+                        res.send({"login":"success","token":token});
+                   }else{
+                       res.send({"login":"fail"});
+                   }
+                }
+            });
+
+           
+        }
+    })
 });
 
 
+//if client token matches with server token
+//client sending the token with headers
+app.get("/products",(req,res)=>{
 
-//create the post request
-app.post("/login",async (req,res)=>{
-    const record = await user.find();
-    console.log(record);
-   if(!record){
-        res.status(400).send({"login":"fail"});
-    }else{
-        res.status(200).send({"login":"success"});
-    }
 });
 
 
@@ -47,3 +69,6 @@ app.post("/login",async (req,res)=>{
 app.listen(8080,()=>{
     console.log("server listening the port number 8080");
 });
+
+
+
