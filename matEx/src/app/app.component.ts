61296516fb7c3e3ad44a09db8ab71e019ea4714c
Sambasivarao <samba.pendela@gmail.com>
2021-12-22 18:42:20 +0530

    code

--- matEx/src/app/app.component.ts
+++ matEx/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
@@ -8,17 +10,43 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class AppComponent {
     title = 'matEx';
+    
+    
+    @ViewChild(MatPaginator,{static:true}) paginator:MatPaginator;
+    @ViewChild(MatSort,{static:true}) sort:MatSort;
+
     displayedColumns:string[] = ["sno","p_id","p_name","p_cost"];
+    
+    
+    
     products:any[] = [
-        {"sno":111,"p_id":"p101","p_name":"p_one","p_cost":10000},
-        {"sno":555,"p_id":"p105","p_name":"p_five","p_cost":50000},
-        {"sno":222,"p_id":"p102","p_name":"p_two","p_cost":20000},
-        {"sno":444,"p_id":"p104","p_name":"p_four","p_cost":40000},
-        {"sno":333,"p_id":"p103","p_name":"p_three","p_cost":30000}
+        {"sno":1,"p_id":"p101","p_name":"p_one","p_cost":10000},
+        {"sno":5,"p_id":"p105","p_name":"p_five","p_cost":50000},
+        {"sno":2,"p_id":"p102","p_name":"p_two","p_cost":20000},
+        {"sno":4,"p_id":"p104","p_name":"p_four","p_cost":40000},
+        {"sno":3,"p_id":"p103","p_name":"p_three","p_cost":30000}
     ];
     dataSource:MatTableDataSource<any>;
 
     constructor(){
+      
+      }
+
+    ngOnInit(){
       this.dataSource = new MatTableDataSource(this.products);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     }
+
+    applyFilter(filterValue: any) {
+
+      this.dataSource.filter = filterValue.value.trim().toLowerCase();
+  
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
+    }
+
+
+
 }
--- matEx/src/app/app.module.ts
+++ matEx/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { MatTableModule } from "@angular/material/table";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
